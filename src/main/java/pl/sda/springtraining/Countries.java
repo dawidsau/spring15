92b2ad306d47afe9b6c0c411b4de5568631e3c16
kf <k.falinski@sdacademy.pl>
2019-05-25 16:07:23 +0200

    spring data

---
+++ src/main/java/pl/sda/springtraining/Countries.java
@@ -0,0 +1,9 @@
+package pl.sda.springtraining;
+
+public enum Countries {
+
+    USA,
+    Poland,
+    Germany,
+    Canada
+}
--- src/main/java/pl/sda/springtraining/User.java
+++ src/main/java/pl/sda/springtraining/User.java
@@ -1,15 +1,26 @@
 package pl.sda.springtraining;
 
+import javax.persistence.*;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
 
     private String firstName;
     private String lastName;
+
+    @Embedded
     private UserAddress userAddress;
     private String birthDate;
     private String pesel;
     private String username;
-    private String password;
+    private String passwordHash;
     private String phone;
+    @Enumerated(EnumType.STRING)
+    private Countries countries;
     private boolean preferEmails;
 
 }
