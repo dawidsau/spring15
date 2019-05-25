ef0b2b93e91beba26d30d22d2e1dbf51abb99ea3
kf <k.falinski@sdacademy.pl>
2019-05-25 14:43:01 +0200

    init

---
+++ src/main/java/pl/sda/springtraining/User.java
@@ -0,0 +1,15 @@
+package pl.sda.springtraining;
+
+public class User {
+
+    private String firstName;
+    private String lastName;
+    private UserAddress userAddress;
+    private String birthDate;
+    private String pesel;
+    private String username;
+    private String password;
+    private String phone;
+    private boolean preferEmails;
+
+}
