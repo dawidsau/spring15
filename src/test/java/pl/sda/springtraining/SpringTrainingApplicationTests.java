ed6088e77b98d2993d07aea13fa2f085afcf11a9
kf <sdakftraining@gmail.com>
2019-06-01 17:03:43 +0200

    test

--- src/test/java/pl/sda/springtraining/SpringTrainingApplicationTests.java
+++ src/test/java/pl/sda/springtraining/SpringTrainingApplicationTests.java
@@ -1,16 +1,42 @@
 package pl.sda.springtraining;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringTrainingApplicationTests {
 
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Before
+    public void setUp() throws Exception {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(SecurityMockMvcConfigurers.springSecurity())
+                .build();
+    }
+
     @Test
-    public void contextLoads() {
+    @WithUserDetails("superUser")
+    public void contextLoads() throws Exception {
+        this.mockMvc
+                .perform(get("/products"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
 }
